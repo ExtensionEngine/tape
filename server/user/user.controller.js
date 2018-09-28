37241c56dfc7e1753018be4d6c3fc9560655f0df
Dario Vladovic <d.vladimyr@gmail.com>
2018-09-28 23:56:47 +0200

    Use Sequelize.EmptyResultError :bulb:

--- server/user/user.controller.js
+++ server/user/user.controller.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const { createError } = require('../common/errors');
-const { Sequelize, User } = require('../common/database');
+const { User, Integration, Sequelize } = require('../common/database');
 const HttpStatus = require('http-status');
 const map = require('lodash/map');
 const pick = require('lodash/pick');
 
-const { BAD_REQUEST, NOT_FOUND } = HttpStatus;
+const { EmptyResultError, Op } = Sequelize;
+const { BAD_REQUEST, FORBIDDEN, NOT_FOUND } = HttpStatus;
 const inputAttrs = ['email', 'role', 'firstName', 'lastName'];
-const Op = Sequelize.Op;
 
 function list({ query: { email, emailLike, role } }, res) {
   const cond = [];
@@ -22,15 +22,15 @@ function list({ query: { email, emailLike, role } }, res) {
 function create(req, res) {
   const { body } = req;
   const origin = req.origin();
-  return User.findOne({ where: { email: body.email } })
-    .then(user => !user || createError(NOT_FOUND, 'User already exists!'))
-    .then(() => User.invite(pick(body, inputAttrs), { origin }))
+  return User.findOne({ where: { email: body.email }, rejectOnEmpty: true })
+    .then(() => createError(FORBIDDEN, 'User already exists!'))
+    .catch(EmptyResultError, () => User.invite(pick(body, inputAttrs), { origin }))
     .then(user => res.jsend.success(user.profile));
 }
 
 function patch({ params, body }, res) {
-  return User.findById(params.id, { paranoid: false })
-    .then(user => user || createError(NOT_FOUND, 'User does not exist!'))
+  return User.findById(params.id, { paranoid: false, rejectOnEmpty: true })
+    .catch(EmptyResultError, () => createError(NOT_FOUND, 'User does not exist!'))
     .then(user => user.update(pick(body, inputAttrs)))
     .then(user => res.jsend.success(user.profile));
 }
@@ -41,8 +41,8 @@ function login({ body }, res) {
     return createError(BAD_REQUEST, 'Please enter email and password!');
   }
 
-  return User.find({ where: { email } })
-    .then(user => user || createError(NOT_FOUND, 'User does not exist!'))
+  return User.find({ where: { email }, rejectOnEmpty: true })
+    .catch(EmptyResultError, () => createError(NOT_FOUND, 'User does not exist!'))
     .then(user => user.authenticate(password))
     .then(user => user || createError(NOT_FOUND, 'Wrong password!'))
     .then(user => {
@@ -54,16 +54,16 @@ function login({ body }, res) {
 function forgotPassword(req, res) {
   const { email } = req.body;
   const origin = req.origin();
-  return User.find({ where: { email } })
-    .then(user => user || createError(NOT_FOUND, 'User not found!'))
+  return User.find({ where: { email }, rejectOnEmpty: true })
+    .catch(EmptyResultError, () => createError(NOT_FOUND, 'User not found!'))
     .then(user => user.sendResetToken({ origin }))
     .then(() => res.end());
 }
 
 function resetPassword({ body, params }, res) {
   const { password, token } = body;
-  return User.find({ where: { token } })
-    .then(user => user || createError(NOT_FOUND, 'Invalid token!'))
+  return User.find({ where: { token }, rejectOnEmpty: true })
+    .catch(EmptyResultError, () => createError(NOT_FOUND, 'Invalid token!'))
     .then(user => {
       user.password = password;
       return user.save();
