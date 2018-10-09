7cbf86ee787d830d2195b09465695b295a405e3e
Damir Perisic <underscope@gmail.com>
2018-10-09 20:18:18 +0200

    Created KnowledgeGraph model

---
+++ server/common/database/migrations/20181008110637-knowledge-graph.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const TABLE_NAME = 'knowledge_graph';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable(TABLE_NAME, {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    repositoryId: {
+      type: Sequelize.INTEGER,
+      field: 'repository_id',
+      allowNull: false
+    },
+    cohortId: {
+      type: Sequelize.SMALLINT,
+      field: 'cohort_id',
+      allowNull: false
+    },
+    nodes: {
+      type: Sequelize.JSONB,
+      allowNull: false
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      field: 'created_at',
+      allowNull: false
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      field: 'updated_at',
+      allowNull: false
+    },
+    deletedAt: {
+      type: Sequelize.DATE,
+      field: 'deleted_at'
+    }
+  }),
+  down: (queryInterface) => queryInterface.dropTable(TABLE_NAME)
+};
