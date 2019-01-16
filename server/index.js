b6d5a369af26124449cc294e6678d313bf7b7adf
Damir Perisic <underscope@gmail.com>
2019-01-16 08:12:41 +0100

    Initialize graph service upon boot

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const bluebird = require('bluebird');
-const sequelize = require('sequelize');
+const graphService = require('./knowledge-graph/GraphService');
 const { promisify } = require('util');
+const sequelize = require('sequelize');
 
 if (process.env.NODE_ENV !== 'production') {
   sequelize.Promise.config({ longStackTraces: true });
@@ -19,6 +20,7 @@ const address = `http://${config.ip}:${config.port}`;
 
 database.initialize()
   .then(migrations => logger.info('🗄️  Database initialized'))
+  .then(() => graphService.initialize(database))
   .then(() => runServer(config.port, config.ip))
   .then(() => logger.info(`✈️  Server listening on ${address}`))
   .catch(err => logger.error({ err }));
