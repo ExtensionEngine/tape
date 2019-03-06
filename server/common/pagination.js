108dd5176c795e439ca92cebf41e663fcd56508d
mcapeta <marin.capeta@gmail.com>
2019-03-06 18:19:03 +0100

    [WIP] Create pagination parser

---
+++ server/common/pagination.js
@@ -0,0 +1,25 @@
+const { utils: { parsePath } } = require('./database');
+const pick = require('lodash/pick');
+
+const parseOptions = ({ limit, offset, sortOrder }) => ({
+  limit: parseInt(limit, 10) || 100,
+  offset: parseInt(offset, 10) || 0,
+  sortOrder: sortOrder || 'ASC'
+});
+
+function setParser(Model) {
+  return (req, _, next) => {
+    const options = parseOptions(req.query);
+    Object.assign(req.query, options);
+    req.options = pick(options, ['limit', 'offset']);
+    const { sortBy } = req.query;
+    if (sortBy) {
+      req.options.order = [[...parsePath(sortBy, Model), options.sortOrder]];
+    }
+    next();
+  };
+}
+
+module.exports = {
+  setParser
+};
