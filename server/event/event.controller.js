f1d5e688c10481cdc7b0393ef1c27769be156443
mcapeta <marin.capeta@gmail.com>
2019-03-07 13:34:05 +0100

    Create `ungradedEvents` route

--- server/event/event.controller.js
+++ server/event/event.controller.js
@@ -1,11 +1,40 @@
-const { GradedEvent, LearnerProfile, UngradedEvent } = require('../common/database');
+const db = require('../common/database');
 const HttpStatus = require('http-status');
+const map = require('lodash/map');
 const pick = require('lodash/pick');
 
+const { GradedEvent, LearnerProfile, UngradedEvent, utils } = db;
+const fn = utils.build(UngradedEvent);
 const { CREATED } = HttpStatus;
 const commonAttrs = ['userId', 'activityId', 'interactionStart', 'interactionEnd'];
 const ungradedAttrs = ['progress'].concat(commonAttrs);
 const gradedAttrs = ['questionId', 'isCorrect', 'answer'].concat(commonAttrs);
+
+const parseResult = it => ({
+  ...it,
+  avgDuration: parseFloat(it.avgDuration),
+  views: parseInt(it.views, 10)
+});
+
+
+function listUngradedEvents({ cohortId, query, options }, res) {
+  const { activityIds, uniqueViews } = query;
+  const group = [fn.column('activityId')];
+  const views = uniqueViews ? fn.distinct('userId') : fn.column('userId');
+  const attributes = [
+    [...group, 'activityId'],
+    [fn.count(views), 'views'],
+    [fn.average('duration'), 'avgDuration'],
+    [fn.max('interactionEnd'), 'lastViewed'],
+  ];
+  const where = { cohortId };
+  if (activityIds) where.activityId = { [Op.in]: activityIds };
+  const opts = { where, ...options, group, attributes, raw: true };
+  return UngradedEvent.findAndCountAll(opts).then(({ rows, count }) => {
+    const items = map(rows, parseResult);
+    return res.jsend.success(({ items, total: count.length }));
+  });
+}
 
 async function reportUngradedEvent({ cohortId, body }, res) {
   const data = { cohortId, ...pick(body, ungradedAttrs) };
@@ -29,6 +58,7 @@ function calculateDuration({ interactionStart, interactionEnd }) {
 }
 
 module.exports = {
+  listUngradedEvents,
   reportUngradedEvent,
   reportGradedEvent
 };
--- server/event/index.js
+++ server/event/index.js
@@ -2,8 +2,11 @@
 
 const ctrl = require('./event.controller');
 const router = require('express').Router();
+const { UngradedEvent } = require('../common/database');
+const { setParser } = require('../common/pagination');
 
 router
+  .get('/ungraded', setParser(UngradedEvent), ctrl.listUngradedEvents)
   .post('/ungraded', ctrl.reportUngradedEvent)
   .post('/graded', ctrl.reportGradedEvent);
 
