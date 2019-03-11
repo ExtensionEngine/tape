6de59862c94773e55a6d4ac206314be96550ff00
mcapeta <marin.capeta@gmail.com>
2019-03-11 16:08:49 +0100

    Fix and add timestamp fields' db level default values

--- server/common/database/migrations/20181006191154-ungraded-event.js
+++ server/common/database/migrations/20181006191154-ungraded-event.js
@@ -43,7 +43,7 @@ module.exports = {
     createdAt: {
       type: Sequelize.DATE,
       field: 'created_at',
-      defaultValue: Sequelize.NOW,
+      defaultValue: Sequelize.fn('NOW'),
       allowNull: false
     }
   }),
