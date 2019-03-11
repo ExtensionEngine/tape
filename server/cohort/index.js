5e13ad6b11d940ffd898c38893a264a726dd4176
mcapeta <marin.capeta@gmail.com>
2019-03-11 14:21:26 +0100

    Create common middleware to parse numeric route params

--- server/cohort/index.js
+++ server/cohort/index.js
@@ -3,15 +3,16 @@
 const ctrl = require('./cohort.controller');
 const event = require('../event');
 const graph = require('../knowledge-graph');
+const { parseNumericParam } = require('../common/middleware');
 const router = require('express').Router();
 
 router
-  .use('/:cohortId*', parseCohortId)
+  .param('cohortId', parseNumericParam)
+  .param('learnerId', parseNumericParam)
   .use('/:cohortId/graph', graph.router)
   .use('/:cohortId/event', event.router)
   .get('/:cohortId/progress', ctrl.getCohortProgress)
   .post('/:cohortId/register', ctrl.registerLearners)
-  .use('/:cohortId/learner/:learnerId*', parseLearnerId)
   .get('/:cohortId/learner/:learnerId', ctrl.getGraph)
   .get('/:cohortId/learner/', ctrl.listGraphs);
 
@@ -19,13 +20,3 @@ module.exports = {
   path: '/cohort',
   router
 };
-
-function parseCohortId(req, _, next) {
-  req.cohortId = parseInt(req.params.cohortId, 10);
-  next();
-}
-
-function parseLearnerId(req, _, next) {
-  req.learnerId = parseInt(req.params.learnerId, 10);
-  next();
-}
