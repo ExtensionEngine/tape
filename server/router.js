fccdda0b226c97e23c240c4ed272229610e2c368
franosincic <fsincic@extensionengine.com>
2019-06-05 13:54:59 +0200

    Add ping server API endpoint

--- server/router.js
+++ server/router.js
@@ -3,8 +3,10 @@
 const auth = require('./common/auth').authenticate('jwt');
 const cohort = require('./cohort');
 const express = require('express');
+const pkg = require('../package.json');
 
 const router = express.Router();
+router.use('/ping', (_, res) => res.jsend.success({ version: pkg.version }));
 router.use(auth);
 router.use(cohort.path, cohort.router);
 
