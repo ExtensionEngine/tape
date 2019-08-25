206381c715c686a5e6488af52a04cd4ace841c0c
mcapeta <marin.capeta@gmail.com>
2019-08-25 18:06:36 +0200

    Name the variable with full name :blowfish:

--- server/common/database/migrations/20190801145621-update-learner-profile.js
+++ server/common/database/migrations/20190801145621-update-learner-profile.js
@@ -4,9 +4,11 @@ const TABLE_NAME = 'learner_profile';
 const COLUMN_NAME = 'excluded';
 
 module.exports = {
-  up: (QI, Sequelize) => QI.addColumn(TABLE_NAME, COLUMN_NAME, {
-    type: Sequelize.BOOLEAN,
-    defaultValue: false
-  }),
-  down: (QI, _) => QI.removeColumn(TABLE_NAME, COLUMN_NAME)
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(TABLE_NAME, COLUMN_NAME, {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    });
+  },
+  down: queryInterface => queryInterface.removeColumn(TABLE_NAME, COLUMN_NAME)
 };
