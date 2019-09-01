604b5a3140e81f93b8b9f5d8df619f5a85cd5e38
mcapeta <marin.capeta@gmail.com>
2019-09-01 02:39:09 +0200

    Move duration aggregation from migration to separate script

--- server/common/database/migrations/20190812233718-add-learner-profile-duration.js
+++ server/common/database/migrations/20190812233718-add-learner-profile-duration.js
@@ -1,42 +1,15 @@
 'use strict';
 
-const db = require('../index');
-const graphService = require('../../../knowledge-graph/graph.service');
-const mapValues = require('lodash/mapValues');
-const omit = require('lodash/omit');
-const Promise = require('bluebird');
-
-const removeDuration = state => mapValues(state, it => omit(it, ['duration']));
-const { LearnerProfile } = db;
-
 const TABLE_NAME = 'learner_profile';
 const COLUMN_NAME = 'duration';
 
 module.exports = {
-  up: async (QI, Sequelize) => {
-    await QI.addColumn(TABLE_NAME, COLUMN_NAME, {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(TABLE_NAME, COLUMN_NAME, {
       type: Sequelize.INTEGER,
       allowNull: false,
       defaultValue: 0
     });
-    const profiles = await LearnerProfile.findAll();
-    await graphService.initialize(db);
-    await Promise.mapSeries(profiles, profile => {
-      const graph = graphService.get(profile.cohortId);
-      const leafNodes = graph.getLeafNodes();
-      return Promise.map(leafNodes, node => profile.aggregateStats(node));
-    });
-    return Promise.mapSeries(profiles, profile => profile.save());
   },
-  down: async (QI, _) => {
-    const profiles = await LearnerProfile.findAll();
-    await Promise.mapSeries(profiles, profile => {
-      const state = removeDuration(profile.state);
-      const repoState = removeDuration(profile.repoState);
-      profile.set('state', state);
-      profile.set('repoState', repoState);
-      return profile.save();
-    });
-    return QI.removeColumn(TABLE_NAME, COLUMN_NAME);
-  }
+  down: queryInterface => queryInterface.removeColumn(TABLE_NAME, COLUMN_NAME)
 };
